6bdf0354914c83f790b39ff20a458a95e45bc39b
Khoa Nguyen <nguyenminhkhoa77284@gmail.com>
2025-04-02 18:22:38 +0900

    hotfix: db connection

--- config/db.js
+++ config/db.js
@@ -1,27 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
-mongoose.set("debug", true); // 👈 This shows all MongoDB queries Mongoose runs
-
 mongoose
-	.connect(process.env.MONGODB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(async () => {
-		console.log("✅ Database connected");
-
-		const dbName = mongoose.connection.name;
-		console.log("📂 Connected to database:", dbName);
-
-		const collections = await mongoose.connection.db
-			.listCollections()
-			.toArray();
-		console.log(
-			"📦 Collections in DB:",
-			collections.map((c) => c.name)
-		);
+	.connect(process.env.MONGODB_URI)
+	.then((res) => {
+		console.log("Database connected");
 	})
 	.catch((error) => {
-		console.error("❌ MongoDB connection error:", error);
+		console.log(error);
 	});
